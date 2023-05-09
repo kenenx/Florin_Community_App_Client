1e86499bb66083b563fb9f227812cfd79be87583
Dohee Lee <dojuhee2015@gmail.com>
2023-05-09 19:02:51 +0100

    finish implementing all the functionality

--- assets/js/post.js
+++ assets/js/post.js
@@ -1,10 +1,14 @@
 const addPosts = document.getElementById('add-posts')
 
 async function viewComplaint() {
-  const response = await fetch('http://localhost:3000/complaints')
-  const data = await response.json()
-  console.log(data)
-  renderData(data)
+  try {
+    const response = await fetch('http://localhost:3000/complaints')
+    const data = await response.json()
+    console.log(data)
+    renderData(data)
+  } catch (error) {
+    console.log(error)
+  }
 }
 
 function renderData(data) {
@@ -16,19 +20,34 @@ function renderData(data) {
     const postTitle = document.createElement('p')
     const postContent = document.createElement('p')
 
+    const writeDate = document.createElement('p')
+    const writeTitle = document.createElement('p')
+    const writeContent = document.createElement('p')
+
+    writeDate.textContent = 'Date:'
+    writeTitle.textContent = 'Title:'
+    writeContent.textContent = 'Details:'
+
     const buttonContainer = document.createElement('div')
     const editButton = document.createElement('button')
     const deleteButton = document.createElement('button')
 
-    postDate.textContent = `Date: ${new Date(post_date).toLocaleDateString()}`
-    postTitle.textContent = `Title: ${title}`
-    postContent.textContent = `Complaint Details: ${content}`
+    postDate.textContent = `${post_date}`
+    postTitle.textContent = `${title}`
+    postContent.textContent = `${content}`
 
     editButton.textContent = 'Edit'
     deleteButton.textContent = 'Delete'
 
+    deleteButton.setAttribute('data-bs-toggle', 'modal')
+    deleteButton.setAttribute('data-bs-target', '#exampleModal')
+    deleteButton.setAttribute('type', 'button')
+
+    postInfo.appendChild(writeDate)
     postInfo.appendChild(postDate)
+    postInfo.appendChild(writeTitle)
     postInfo.appendChild(postTitle)
+    postInfo.appendChild(writeContent)
     postInfo.appendChild(postContent)
 
     buttonContainer.appendChild(editButton)
@@ -50,18 +69,24 @@ function renderData(data) {
       const inputDate = document.createElement('input')
       const inputTitle = document.createElement('input')
       const inputContent = document.createElement('input')
-      const writeDate = document.createElement('p')
-      const writeTitle = document.createElement('p')
-      const writeContent = document.createElement('p')
 
-      writeDate.textContent = 'DATE'
+      const writeDate2 = document.createElement('p')
+      const writeTitle2 = document.createElement('p')
+      const writeContent2 = document.createElement('p')
+
+      writeDate2.textContent = 'Date:'
+      writeTitle2.textContent = 'Title:'
+      writeContent2.textContent = 'Details:'
 
       inputDate.value = postDate.textContent
       inputTitle.value = postTitle.textContent
       inputContent.value = postContent.textContent
 
+      inputContainer.appendChild(writeDate2)
       inputContainer.appendChild(inputDate)
+      inputContainer.appendChild(writeTitle2)
       inputContainer.appendChild(inputTitle)
+      inputContainer.appendChild(writeContent2)
       inputContainer.appendChild(inputContent)
 
       inputDate.type = 'text'
@@ -71,6 +96,10 @@ function renderData(data) {
       postInfo.appendChild(inputContainer)
 
       editButton.dataset.id = post.comp_id
+
+      updateButton.setAttribute('data-bs-toggle', 'modal')
+      updateButton.setAttribute('data-bs-target', '#staticBackdrop')
+      updateButton.setAttribute('type', 'button')
 
       updateButton.addEventListener('click', () => {
         const data = {
@@ -85,31 +114,50 @@ function renderData(data) {
 }
 
 async function deletePost(id) {
-  const options = {
-    method: 'DELETE',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  }
-  const result = await fetch(`http://localhost:3000/complaints/${id}`, options)
-  if (result.status === 204) {
-    window.location.reload()
-  }
+  const yesButton = document.querySelector('.yes-button')
+  const noButton = document.querySelector('.no-button')
+  yesButton.addEventListener('click', async () => {
+    const options = {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    }
+    const result = await fetch(
+      `http://localhost:3000/complaints/${id}`,
+      options
+    )
+    if (result.status === 204) {
+      window.location.reload()
+    }
+  })
+  noButton.addEventListener('click', () => {
+    return
+  })
 }
 
 async function updateEntry(id, data) {
-  const options = {
-    method: 'PATCH',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(data),
-  }
-  console.log(data)
-  const result = await fetch(`http://localhost:3000/complaints/${id}`, options)
-  if (result.status === 200) {
-    window.location.reload()
-  }
+  const yes2Button = document.querySelector('.yes2-button')
+  const no2Button = document.querySelector('.no2-button')
+  yes2Button.addEventListener('click', async () => {
+    const options = {
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    }
+    const result = await fetch(
+      `http://localhost:3000/complaints/${id}`,
+      options
+    )
+    if (result.status === 200) {
+      window.location.reload()
+    }
+  })
+  no2Button.addEventListener('click', () => {
+    return
+  })
 }
 
 viewComplaint()
