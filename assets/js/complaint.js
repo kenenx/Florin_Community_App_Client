1034c019c5f851b85a7c107e1ede7a835200fc41
Dohee Lee <dojuhee2015@gmail.com>
2023-05-10 15:49:36 +0100

    add some changes

--- assets/js/complaint.js
+++ assets/js/complaint.js
@@ -1,16 +1,19 @@
 const form = document.getElementById('complaints-form')
-// const renderEntry = document.querySelector('.post div')
 
 form.addEventListener('submit', submitForm)
 
 async function submitForm(event) {
   event.preventDefault()
 
+  // Disable the submit button
+  const submitButton = form.querySelector('button[type="submit"]')
+  submitButton.disabled = true
+
   const add = {
     title: event.target.title.value,
     post_date: event.target.date.value,
     content: event.target.content.value,
-    user_id: parseInt(Math.floor(Math.random() * 10)),
+    user_id: 1,
   }
 
   const options = {
@@ -20,14 +23,52 @@ async function submitForm(event) {
     },
     body: JSON.stringify(add),
   }
-
-  console.log(add)
-  const result = await fetch('http://localhost:3000/complaints', options)
-  console.log(result)
-  if (result.status === 201) {
-    event.target.title.value = ''
-    event.target.date.value = ''
-    event.target.content.value = ''
-    window.location.assign('../../post.html')
+  try {
+    const result = await fetch('http://localhost:3000/complaints', options)
+    console.log(result)
+    if (result.status === 201) {
+      event.target.title.value = ''
+      event.target.date.value = ''
+      event.target.content.value = ''
+      window.location.assign('../../post.html')
+    }
+  } catch (error) {
+    console.error(error)
+  } finally {
+    // Re-enable the submit button
+    submitButton.disabled = false
   }
 }
+
+// const form = document.getElementById('complaints-form')
+// // const renderEntry = document.querySelector('.post div')
+
+// form.addEventListener('submit', submitForm)
+
+// async function submitForm(event) {
+//   event.preventDefault()
+
+//   const add = {
+//     title: event.target.title.value,
+//     post_date: event.target.date.value,
+//     content: event.target.content.value,
+//     user_id: parseInt(Math.floor(Math.random() * 10)),
+//   }
+
+//   const options = {
+//     method: 'POST',
+//     headers: {
+//       'Content-Type': 'application/json',
+//     },
+//     body: JSON.stringify(add),
+//   }
+
+//   const result = await fetch('http://localhost:3000/complaints', options)
+
+//   if (result.status === 201) {
+//     event.target.title.value = ''
+//     event.target.date.value = ''
+//     event.target.content.value = ''
+//     window.location.assign('../../post.html')
+//   }
+// }
--- assets/js/userprofile.js
+++ assets/js/userprofile.js
@@ -1,30 +1,40 @@
-function binCollData(data) {
-  const bin = document.createElement('div')
-  bin.className = 'bininfo'
-
-  const date = document.createElement('h2')
-  date.textContent = data['bin_id']
-  bin.appendChild(date)
-
-  return bin
-}
-
-async function loadbinCollData() {
+async function loadComplaints() {
   const options = {
     headers: {
       Authorization: localStorage.getItem('token'),
     },
   }
-  const response = await fetch('http://localhost:3000/users/:id/bin', options)
+  let user_id = 1
+  const response = await fetch(
+    `http://localhost:3000/users/profile/${user_id}/complaints`,
+    options
+  )
 
   if (response.status === 200) {
     const data = await response.json()
-    const container = document.getElementById('bininfo')
-    console.log(data.bin_id)
-    container.innerHTML = data.bin_id
+    renderData(data)
   } else {
-    //window.location.assign("./index.html");
+    window.location.assign('./index.html')
   }
 }
 
-loadbinCollData()
+function renderData(data) {
+  data.forEach((post) => {
+    const { post_date, title, content } = post
+
+    const container = document.getElementById('complaintsInfo')
+    const postTitle = document.createElement('p')
+    const date = document.createElement('p')
+    const postContent = document.createElement('p')
+
+    date.textContent = post_date
+    postTitle.textContent = title
+    postContent.textContent = content
+
+    container.appendChild(date)
+    container.appendChild(postTitle)
+    container.appendChild(postContent)
+  })
+}
+
+loadComplaints()
