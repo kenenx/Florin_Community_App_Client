bdfa21a96ff095950134c90595f30106cfccc153
N French :) <72806068+NaNNaNNaNNaNBoolean@users.noreply.github.com>
2023-05-11 09:38:20 +0100

    testing layout

--- __tests__/layout.spec.js
+++ __tests__/layout.spec.js
@@ -48,3 +48,49 @@ describe('register.html',()=>{
         expect(input).toBeTruthy()
     })
 })
+
+describe('login.html',()=>{
+    beforeEach(async () => {
+        dom = await renderDOM('./login.html')
+        document = await dom.window.document
+    })
+
+    it('has a input user name', () => {
+        const from = document.querySelector('form')
+        const input = document.querySelectorAll('#username')
+        expect(input).toBeTruthy()
+    })
+
+    it('has input password', () => {
+        const from = document.querySelector('form')
+        const input = document.querySelectorAll('#password')
+        expect(input).toBeTruthy()
+    })
+})
+
+describe('mainpage.html',()=>{
+    beforeEach(async () => {
+        dom = await renderDOM('./mainpage.html')
+        document = await dom.window.document
+    })
+    it('has a navbar', () => {
+        const nav = document.querySelector('nav')
+        expect(nav).toBeTruthy()
+    })
+    it('has a recycling button', () => {
+        const btn = document.querySelector('#recyclebtn')
+        expect(btn).toBeTruthy()
+    })
+    it('has a events button', () => {
+        const btn = document.querySelectorAll('#eventbtn')
+        expect(btn).toBeTruthy()
+    })
+
+    it('has a complaints button', () => {
+        const btn = document.querySelectorAll('#complaintsbtn')
+        expect(btn).toBeTruthy()
+    })
+})
+
+
+
